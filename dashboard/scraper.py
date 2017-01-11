af04e936e6a34bbfd569d90d14b8a02caff36400
MirjaLagerwaard <mirja_1994@hotmail.com>
2017-01-11 23:37:52 +0100

    Finished scraping

--- dashboard/scraper.py
+++ dashboard/scraper.py
@@ -10,10 +10,11 @@ import csv
 from pattern.web import URL, DOM
 import pattern.web as web
 
-TARGET_URL = "https://syrianarchive.org/database/"
+TARGET_URL = "https://syrianarchive.org/database/?page="
+INCIDENT_URL = "https://syrianarchive.org"
 BACKUP_HTML = 'syrianarchive.html'
 OUTPUT_CSV = 'syrianarchive.csv'
-
+PAGES = 55
 
 def extract_incidents(dom):
     """
@@ -25,16 +26,24 @@ def extract_incidents(dom):
 
     print dom.by_tag('tr')
 
-    for pages in range(50):
-        for incident in dom.by_tag('tr'):
-            if i > 0:
-                link = incident.by_tag('a')[0].content
-                description = incident.by_tag('div')[0].content[1:]
-                date = incident.by_tag('td')[2].content[1:]
-                location = incident.by_tag('td')[3].content[1:]
-                violation = incident.by_tag('td')[4].content[1:]
-                incident_list.append([link.encode('utf-8'), description.encode('utf-8'), date.encode('utf-8'), location.encode('utf-8'), violation.encode('utf-8')])
-            i += 1
+    for incident in dom.by_tag('tr'):
+        if i > 0:
+            link = INCIDENT_URL + incident.by_tag('a')[0].href
+
+            url = URL(link)
+            html = url.download(timeout=100)
+            dom_incident = DOM(html)
+
+            weapons = [weapon.strip() for weapon in dom_incident.by_tag('p')[16].content[27:].split('<br />')]
+            weapons = ", ".join(weapons)[:-2]
+
+            description = incident.by_tag('div')[0].content[1:].strip()
+            date = incident.by_tag('td')[2].content[1:].strip()
+            location = incident.by_tag('td')[3].content[1:].strip()
+            violation = incident.by_tag('td')[4].content[1:].strip()
+            incident_list.append([link.encode('utf-8'), weapons.encode('utf-8'), description.encode('utf-8'), date.encode('utf-8'), location.encode('utf-8'), violation.encode('utf-8')])
+
+        i += 1
 
     return incident_list
 
@@ -43,15 +52,15 @@ def save_csv(f, incidents):
     TODO
     '''
     writer = csv.writer(f)
-    writer.writerow(['link', 'description', 'date', 'location', 'violation'])
+    writer.writerow(['link', 'wapons', 'description', 'date', 'location', 'violation'])
 
     for row in incidents:
         writer.writerow(row)
 
 if __name__ == '__main__':
     # Download the HTML file
-    url = URL(TARGET_URL)
-    html = url.download()
+    url = URL(TARGET_URL + '1')
+    html = url.download(timeout=100)
 
     # Save a copy to disk in the current directory, this serves as an backup
     # of the original HTML, will be used in grading.
@@ -59,11 +68,16 @@ if __name__ == '__main__':
         f.write(html)
 
     # Parse the HTML file into a DOM representation
-    dom = DOM(html)
+    incidents = []
+    for i in range(1, PAGES + 1):
+        url = URL(TARGET_URL + str(i))
+        html = url.download(timeout=100)
+        dom = DOM(html)
 
-    # Extract the tv series (using the function you implemented)
-    incidents = extract_incidents(dom)
-    print incidents
+        # Extract the tv series (using the function you implemented)
+        incidents += extract_incidents(dom)
+        # print incidents
+        print i
 
     # Write the CSV file to disk (including a header)
     with open(OUTPUT_CSV, 'wb') as output_file:
